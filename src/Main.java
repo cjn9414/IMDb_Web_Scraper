5547cc7663310d795d3bdf6edb17ae2be68a02b3
Carter Nesbitt <ccnesbitt@gmail.com>
2018-06-13 13:08:23 -0400

    Developed automated login

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import Utils.Scraper;
 import org.jsoup.nodes.Document;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    
+    static final int exampleCount = 1; // number of scrapes
     private static Document site;
 
     public static void main(String[] args) {
-        Scraper scraper = new Scraper(new InputStreamReader(System.in));
-        scraper.scrape();
+        String view = "https://www.linkedin.com/";
+        for (int i = 0; i < exampleCount; i++) {
+            Scraper scraper = new Scraper(new InputStreamReader(new ByteArrayInputStream(view.getBytes(StandardCharsets.UTF_8))));
+            //scraper.scrapeData();
+            //view = scraper.getNextPage();
+            try {
+                scraper.executeLogin();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/Utils/Scraper.java
+++ src/Utils/Scraper.java
@@ -1,10 +1,14 @@
 package Utils;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Scraper {
     BufferedReader reader;
@@ -14,16 +18,80 @@ public class Scraper {
         this.reader = new BufferedReader(website);
     }
 
-    public void scrape() {
+    /*
+    This function will compile all necessary data from a given webpage. This data we are collecting consists
+    of any given feature we feel will be advantageous to analyze.
+     */
+
+    public void scrapeData() {
         try {
-            site = Jsoup.connect(reader.readLine()).timeout(0).get();
 
             Elements tags = site.select("tr");
             for (int index = 0; index < tags.size(); index++) {
                 System.out.println(tags.get(index).text());
             }
-        } catch (Exception error) {
-            error.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    /*
+    This function will obtain the next page we will scrape. It will do so by finding a link to
+    a new page, from the page it is currently on.
+     */
+
+    public String getNextPage() {
+        String newSite = "";
+        try {
+            site = Jsoup.connect(reader.readLine()).timeout(0).get();
+            Elements newPeople = site.getElementsByClass("name actor-name");
+            for (Element name : newPeople) {
+                System.out.println(name.text());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newSite;
+    }
+
+    /*
+    This function performs the login which is required upon connecting to the website.
+     */
+
+    public void executeLogin() throws IOException {
+
+        final String user_agent = "Mozilla";
+        String loginFormURL = "https://www.linkedin.com/";
+        String loginActionURL = "https://www.linkedin.com/uas/login-submit";
+        String username = "Enter email"; // enter your own email
+        String password = "Enter password"; // enter your own password
+
+        HashMap<String, String> cookies = new HashMap<>();
+        HashMap<String, String> formData = new HashMap<>();
+        Connection.Response loginForm = Jsoup.connect(loginFormURL)
+                .method(Connection.Method.GET)
+                .userAgent(user_agent)
+                .execute();
+
+        Document loginDoc = loginForm.parse();
+        cookies.putAll(loginForm.cookies());
+        String login_csrf_param = loginDoc.select("#loginCsrfParam-login")
+                .first()
+                .attr("value");
+
+
+        formData.put("session_key", username);
+        formData.put("session_password", password);
+        formData.put("IsJsEnabled", "false");
+        formData.put("loginCsrfParam", login_csrf_param);
+
+        Connection.Response homePage = Jsoup.connect(loginActionURL)
+                .cookies(cookies)
+                .data(formData)
+                .method(Connection.Method.POST)
+                .userAgent(user_agent)
+                .execute();
+
+    }
+
 }
